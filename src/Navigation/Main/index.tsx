f9da4c288ff27624a9d36e15c8d309639bb9365c
Lam Nhat Tan <tan.lamnhat@elca.vn>
2023-11-09 23:58:44 +0700

    Bottom navbar

--- src/Navigation/Main/index.tsx
+++ src/Navigation/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeContainer } from "@/Screens/Home";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,10 +10,42 @@ export const MainNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="Home"
+        name="Recipes"
         component={HomeContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="text-box-multiple-outline" color={color} size={size} />
+          ),
+          tabBarLabelPosition: "beside-icon",
+        }}
+      />
+      <Tab.Screen
+        name="Scan"
+        component={HomeContainer}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="scan-helper" color={color} size={size} />
+          ),
+          tabBarLabelPosition: "beside-icon",
+        }}
+      />
+       <Tab.Screen
+        name="Cart"
+        component={HomeContainer}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cart" color={color} size={size} />
+          ),
+          tabBarLabelPosition: "beside-icon",
+        }}
+      />
+       <Tab.Screen
+        name="Profile"
+        component={HomeContainer}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
           tabBarLabelPosition: "beside-icon",
         }}
       />
